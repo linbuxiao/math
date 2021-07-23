c82b97a44be93a36debb484f533a5faa26874559
linbuxiao <linbuxiao@gmail.com>
2021-07-23 23:09:49 +0800

    feat: 对称二叉树 序列化解法

--- example/index.ts
+++ example/index.ts
@@ -1,6 +1,6 @@
-import { maxDepth } from "../src/Tree/最大深度";
+import { isSymmetric } from "../src/Tree/对称二叉树";
 import { turnTreeToArray } from "../src/Utils";
 
-
-console.log(maxDepth(turnTreeToArray([1,2,3,4,5])));
-
+console.log(
+  isSymmetric(turnTreeToArray([2, 3, 3, 4, 5, 5, 4, null, null, 8, 9, 9, 8]))
+);
--- src/Tree/对称二叉树.ts
+++ src/Tree/对称二叉树.ts
@@ -4,11 +4,38 @@
 
 import { TreeNode } from "../Utils";
 
+// 序列化对比法
 export function isSymmetric(root: TreeNode | null): boolean {
-  if(root === null) return false
+  // 遍历左树，并序列化到转化为字符串。
+  // 遍历右数，期间反转所有左右树，判断是否相等
 
-  if(isSymmetric(root.left) === isSymmetric(root.right)) {
-    return true
+  if (root === null) return true;
+
+  const dfsHelper = (root: TreeNode | null, reverse = false) => {
+    let node = "";
+    if (!root) return node;
+    node = dfs(root, reverse);
+    return node;
+  };
+
+  const dfs = (root: TreeNode | null, reverse = false): string => {
+    if (root === null) return "";
+
+    const left = dfsHelper(root.left, reverse);
+    const right = dfsHelper(root.right, reverse);
+
+    if (reverse) {
+      return `root${root.val}-left${right}-right${left}`;
+    }
+    return `root${root.val}-left${left}-right${right}`;
+  };
+
+  console.log(dfs(root.left));
+  console.log(dfs(root.right, true));
+
+  if (dfs(root.left) === dfs(root.right, true)) {
+    return true;
   }
-  return false
-};
+
+  return false;
+}
