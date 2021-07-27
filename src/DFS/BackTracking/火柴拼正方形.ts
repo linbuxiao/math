070f7d50b0799000f22e65de31e8360832cc0f2b
codefactor-io <support@codefactor.io>
2021-07-27 10:00:41 +0000

    [CodeFactor] Apply fixes to commit 8de2293

--- src/DFS/BackTracking/火柴拼正方形.ts
+++ src/DFS/BackTracking/火柴拼正方形.ts
@@ -1,7 +1,7 @@
 /**
  * 还记得童话《卖火柴的小女孩》吗？现在，你知道小女孩有多少根火柴，请找出一种能使用所有火柴拼成一个正方形的方法。不能折断火柴，可以把火柴连接起来，并且每根火柴都要用到。
  * 输入为小女孩拥有火柴的数目，每根火柴用其长度表示。输出即为是否能用所有的火柴拼成正方形。
- * 
+ *
  * 输入: [1,1,2,2,2]
  * 输出: true
  */
@@ -23,7 +23,7 @@ export function makesquare(matchsticks: number[]): boolean {
       return (
         sideArr[0] === sideArr[1] &&
         sideArr[1] === sideArr[2] &&
-        sideArr[2] === sideArr[3] 
+        sideArr[2] === sideArr[3]
       )
     }
 
