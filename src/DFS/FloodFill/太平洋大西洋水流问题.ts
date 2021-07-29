bd5a4535afb94c93c300317ef8acc20892c56f3d
biwenliang <linbuxiao@gmail.com>
2021-07-29 17:57:13 +0800

    feat: 新增练习

--- src/DFS/FloodFill/太平洋大西洋水流问题.ts
+++ src/DFS/FloodFill/太平洋大西洋水流问题.ts
@@ -4,34 +4,78 @@
  * 请找出那些水流既可以流动到“太平洋”，又能流动到“大西洋”的陆地单元的坐标。
  */
 
-
+// left 左侧和顶部太平洋
+// right 右侧和底部大西洋
 export function pacificAtlantic(heights: number[][]): number[][] {
   const result = []
-  const dfs = (row: number, col: number, pacific: boolean, atlantic: boolean, used: boolean[][]) => {
-    if(row === 0 || col === 0) {
-      pacific = true
+  const dfs = (row: number, col: number, used: boolean[][]) => {
+
+    let left: boolean = false
+    let right: boolean = false
+
+    for(let x = 0; x<heights[0].length; x++) {
+      if(used[0][x]) {
+        left = true
+      }
+
+      if(used[used.length - 1][x]) {
+        right = true
+      }
     }
-    
-    if(row === heights.length - 1 || col === heights[0].length - 1) {
-      atlantic = true
+
+    for(let y = 0; y<heights.length; y++) {
+      if(used[y][0]) {
+        left = true
+      }
+
+      if(used[y][used[0].length - 1]) {
+        right = true
+      }
     }
-    
-    if(pacific && atlantic) {
-      return true
-    }
+
+    if(left && right) return true
+    // if(left || right) {
+    //   for(let x = 0; x<used[0].length; x++) {
+    //     if(left && x !== used[0].length - 1) {
+    //       used[0][x] = true
+    //     }
+        
+    //     if(right && x !== 0) {
+    //       used[used.length - 1][x] = true
+    //     }
+    //   }
+
+    //   for(let y = 0; y<used.length; y++) {
+    //     if(left && y !== used.length - 1) {
+    //       used[y][0] = true
+    //     }
+        
+    //     if(right && y !== 0) {
+    //       used[y][used[0].length - 1] = true
+    //     }
+    //   }
+    // }
+
     
     const val = heights[row][col]
     if(row > 0 && heights[row - 1][col] <= val) {
       if(!used[row - 1][col]) {
         used[row - 1][col] = true
-        if(dfs(row - 1, col, pacific, atlantic, used)) return true
+        if(dfs(row - 1, col, used)) return true
+      }
+    }
+
+    if(col < heights[0].length - 1 && heights[row][col + 1] <= val) {
+      if(!used[row][col + 1]) {
+        used[row][col + 1] = true
+        if(dfs(row, col + 1, used)) return true
       }
     }
 
     if(row < heights.length - 1 && heights[row + 1][col] <= val) {
       if(!used[row + 1][col]) {
         used[row + 1][col] = true
-        if(dfs(row + 1, col, pacific, atlantic, used)) return true
+        if(dfs(row + 1, col, used)) return true
       }
       
     }
@@ -39,16 +83,11 @@ export function pacificAtlantic(heights: number[][]): number[][] {
     if(col > 0 && heights[row][col - 1] <= val) { 
       if(!used[row][col - 1]) {
         used[row][col - 1] = true
-        if(dfs(row, col - 1, pacific, atlantic, used)) return true
+        if(dfs(row, col - 1, used)) return true
       }
     }
 
-    if(col < heights[0].length - 1 && heights[row][col + 1] <= val) {
-      if(!used[row][col + 1]) {
-        used[row][col + 1] = true
-        if(dfs(row, col + 1, pacific, atlantic, used)) return true
-      }
-    }
+
 
     return false
   }
@@ -63,8 +102,12 @@ export function pacificAtlantic(heights: number[][]): number[][] {
         }
         used.push(r)
       }
+      
+      used[row][col] = true
+      if(dfs(row, col, used)) {
+        result.push([row, col])
+      } 
 
-      if(dfs(row, col, false, false, used)) result.push([row, col])
     }
   }
   return result
---
+++ src/DFS/FloodFill/统计封闭岛屿的数目.ts
@@ -0,0 +1,75 @@
+/**
+ * 有一个二维矩阵 grid ，每个位置要么是陆地（记号为 0 ）要么是水域（记号为 1 ）。
+ * 我们从一块陆地出发，每次可以往上下左右 4 个方向相邻区域走，能走到的所有陆地区域，我们将其称为一座「岛屿」。
+ * 如果一座岛屿 完全 由水域包围，即陆地边缘上下左右所有相邻区域都是水域，那么我们将其称为 「封闭岛屿」。
+ * 请返回封闭岛屿的数目。
+ */
+
+export function closedIsland(grid: number[][]): number {
+  let num = 0
+  const used: boolean[][] = []
+  const path = []
+  for(let y = 0; y<grid.length; y++) {
+    let r = []
+    for(let x = 0; x<grid[0].length; x++) {
+      r.push(false)
+    } 
+    used.push(r)
+  }
+
+  
+
+  const dfs = (row: number, col: number) => {
+    used[row][col] = true
+    if(row === 0 || row === grid.length - 1 || col === 0 || col === grid[0].length - 1) {
+      console.log(row, col);
+      
+      return false
+    }
+
+    if(row > 0 && grid[row - 1][col] === 0) {
+      if(!used[row - 1][col]) {
+        // if(dfs(row - 1, col)) return true
+        dfs(row - 1, col)
+      }
+    }
+
+    if(row < grid.length - 1 && grid[row + 1][col] === 0) {
+      if(!used[row + 1][col]) {
+        // if(dfs(row + 1, col)) return true
+        dfs(row + 1, col)
+      }
+    }
+
+    if(col > 0 && grid[row][col - 1] === 0) {
+      if(!used[row][col - 1]) {
+        // if(dfs(row, col -1)) return true
+        dfs(row, col -1)
+      }
+    }
+
+    if(col < grid[0].length - 1 && grid[row][col + 1] === 0) {
+      if(!used[row][col + 1]) {
+        // if(dfs(row, col + 1)) return true
+        dfs(row, col + 1)
+      }
+    }
+    
+    return true
+  }
+
+  // 无需遍历最外层
+  for(let y = 1; y<grid.length - 1; y++) {
+    for(let x = 1; x<grid[0].length - 1; x++) {
+      if(!used[y][x] && grid[y][x] === 0) {
+        if(dfs(y, x)) {
+          path.push([y,x])
+          num++
+        }
+      }
+    }
+  }
+  console.log(path);
+  
+  return num
+};
