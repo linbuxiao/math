5ae447a69ef689d8b7d6a9bd0ca8477a580fadd9
biwenliang <linbuxiao@gmail.com>
2021-07-29 17:58:22 +0800

    test: lint

--- src/DFS/FloodFill/统计封闭岛屿的数目.ts
+++ src/DFS/FloodFill/统计封闭岛屿的数目.ts
@@ -6,70 +6,73 @@
  */
 
 export function closedIsland(grid: number[][]): number {
-  let num = 0
-  const used: boolean[][] = []
-  const path = []
-  for(let y = 0; y<grid.length; y++) {
-    let r = []
-    for(let x = 0; x<grid[0].length; x++) {
-      r.push(false)
-    } 
-    used.push(r)
+  let num = 0;
+  const used: boolean[][] = [];
+  const path = [];
+  for (let y = 0; y < grid.length; y++) {
+    let r = [];
+    for (let x = 0; x < grid[0].length; x++) {
+      r.push(false);
+    }
+    used.push(r);
   }
-
-  
 
   const dfs = (row: number, col: number) => {
-    used[row][col] = true
-    if(row === 0 || row === grid.length - 1 || col === 0 || col === grid[0].length - 1) {
+    used[row][col] = true;
+    if (
+      row === 0 ||
+      row === grid.length - 1 ||
+      col === 0 ||
+      col === grid[0].length - 1
+    ) {
       console.log(row, col);
-      
-      return false
+
+      return false;
     }
 
-    if(row > 0 && grid[row - 1][col] === 0) {
-      if(!used[row - 1][col]) {
+    if (row > 0 && grid[row - 1][col] === 0) {
+      if (!used[row - 1][col]) {
         // if(dfs(row - 1, col)) return true
-        dfs(row - 1, col)
+        dfs(row - 1, col);
       }
     }
 
-    if(row < grid.length - 1 && grid[row + 1][col] === 0) {
-      if(!used[row + 1][col]) {
+    if (row < grid.length - 1 && grid[row + 1][col] === 0) {
+      if (!used[row + 1][col]) {
         // if(dfs(row + 1, col)) return true
-        dfs(row + 1, col)
+        dfs(row + 1, col);
       }
     }
 
-    if(col > 0 && grid[row][col - 1] === 0) {
-      if(!used[row][col - 1]) {
+    if (col > 0 && grid[row][col - 1] === 0) {
+      if (!used[row][col - 1]) {
         // if(dfs(row, col -1)) return true
-        dfs(row, col -1)
+        dfs(row, col - 1);
       }
     }
 
-    if(col < grid[0].length - 1 && grid[row][col + 1] === 0) {
-      if(!used[row][col + 1]) {
+    if (col < grid[0].length - 1 && grid[row][col + 1] === 0) {
+      if (!used[row][col + 1]) {
         // if(dfs(row, col + 1)) return true
-        dfs(row, col + 1)
+           dfs(row, col + 1);
       }
     }
-    
-    return true
-  }
+
+    return true;
+  };
 
   // 无需遍历最外层
-  for(let y = 1; y<grid.length - 1; y++) {
-    for(let x = 1; x<grid[0].length - 1; x++) {
-      if(!used[y][x] && grid[y][x] === 0) {
-        if(dfs(y, x)) {
-          path.push([y,x])
-          num++
+  for (let y = 1; y < grid.length - 1; y++) {
+    for (let x = 1; x < grid[0].length - 1; x++) {
+      if (!used[y][x] && grid[y][x] === 0) {
+        if (dfs(y, x)) {
+          path.push([y, x]);
+          num++;
         }
       }
     }
   }
   console.log(path);
-  
-  return num
-};
+
+  return num;
+}
