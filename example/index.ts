659c53a9756e1d45b3da38636a3d973c38143be8
biwenliang <linbuxiao@gmail.com>
2021-08-09 11:45:41 +0800

    feat(practice): 🎸 day practice

--- example/index.ts
+++ example/index.ts
@@ -1,3 +1,10 @@
-import { tribonacci } from "../src/Day/August/第 N 个泰波那契数";
-
-tribonacci(37);
+import { nthSuperUglyNumber } from "../src/Day/August/超级丑数";
+console.log(
+  nthSuperUglyNumber(
+    800,
+    [
+      37, 43, 59, 61, 67, 71, 79, 83, 89, 97, 101, 103, 113, 127, 131, 157, 163,
+      167, 173, 179, 191, 193, 197, 199, 211, 229, 233, 239, 251, 257,
+    ]
+  )
+);
