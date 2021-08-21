c532b32cedd9b3d24d594b47699d1c492e329a9d
linbuxiao <linbuxiao@gmail.com>
2021-08-21 14:02:05 +0800

    feat(practice): 🎸 days

--- example/index.ts
+++ example/index.ts
@@ -1,7 +1,7 @@
-import { openLock } from "../src/BPS/TwoWay/打开转盘锁";
+import { slidingPuzzle } from "../src/BPS/TwoWay/滑动谜题";
 console.log(
-  openLock(
-    ["8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"],
-    "8888"
-  )
+  slidingPuzzle([
+    [1, 2, 3],
+    [4, 0, 5],
+  ])
 );
