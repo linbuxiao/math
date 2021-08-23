e786ccd411d4cecbd9cd1b854cb90f3fc16c925f
biwenliang <linbuxiao@gmail.com>
2021-08-23 09:08:18 +0800

    feat(practice): 🎸 days

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,3 @@
-import { maxSumSubmatrix } from "../src/DP/矩形区域不超过 K 的最大数值和";
+import { getMaximumGenerated } from "../src/Day/August/获取生成数组中的最大值";
 
-console.log(
-  maxSumSubmatrix(
-    [
-      [1, 0, 1],
-      [0, -2, 3],
-    ],
-    2
-  )
-);
+getMaximumGenerated(7);
