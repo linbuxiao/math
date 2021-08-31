fe13a25d67d88c85ca02fc1dcd3c07652bf04a29
biwenliang <linbuxiao@gmail.com>
2021-08-31 10:07:12 +0800

    feat(practice): 🎸 days

---
+++ example/index.ts
@@ -0,0 +1,11 @@
+import { corpFlightBookings } from "../src/Day/August/航班预订统计";
+console.log(
+  corpFlightBookings(
+    [
+      [1, 2, 10],
+      [2, 3, 20],
+      [2, 5, 25],
+    ],
+    5
+  )
+);
