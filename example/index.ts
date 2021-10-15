7cf8314d18b9fe1209a9372ef2ddd23a4ff02200
biwenliang <linbuxiao@gmail.com>
2021-10-15 09:38:52 +0800

    test(practice): 💍 days

--- example/index.ts
+++ example/index.ts
@@ -1,2 +1,3 @@
-import { peakIndexInMountainArray } from "../src/Day/October/剑指 Offer II 069. 山峰数组的顶部";
-console.log(peakIndexInMountainArray([18, 29, 38, 59, 98, 100, 99, 98, 90]));
+import { countAndSay } from "../src/Day/October/外观数列";
+
+countAndSay();
